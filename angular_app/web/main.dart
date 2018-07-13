41c1432e0da2d5f3d250b779aacf2a8173509364
aman <amangary@uw.edu>
2018-07-13 16:45:39 -0700

    Trying to add PWA

--- angular_app/web/main.dart
+++ angular_app/web/main.dart
@@ -13,7 +13,6 @@ final InjectorFactory injector = self.injector$Injector;
 
 void main() {
   fb.initializeApp(
-    apiKey: "AIzaSyAH5audK7N3Xd1028LmjUDtm5Fe8CPbxv0",
     authDomain: "library-579a7.firebaseapp.com",
     databaseURL: "https://library-579a7.firebaseio.com",
     projectId: "library-579a7",
